d4503cc4662172e8451696a86b9becc8bb429221
Vivek Verma <vivnps.verma@gmail.com>
2019-03-06 21:18:02 -0800

    Adding

---
+++ app/src/main/java/com/example/navblind/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.navblind;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.highgui.Highgui;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOError;
+
+import static org.opencv.core.CvType.CV_8UC3;
+
+
+public class MainActivity extends AppCompatActivity  implements  CameraBridgeViewBase.CvCameraViewListener2{
+
+    private static final String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat frame;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
+
+//        static {
+////            System.loadLibrary("openCVLibrary24136");
+//            System.loadLibrary("openCVLibrary24136");
+//        }
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV initialize success");
+        } else {
+            Log.i(TAG, "OpenCV initialize failed");
+        }
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    javaCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},CAMERA_PERMISSION_REQUEST_CODE);
+        }
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(View.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView!=null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void  onResume(){
+
+        super.onResume();
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV loaded successfully.");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.i(TAG, "OpenCV not loaded.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, this, mLoaderCallback);
+        }
+    }
+
+    //        @Override
+    public void onCameraViewStarted(int width, int height) {
+        frame=new Mat(height,width, CV_8UC3);
+
+    }
+
+    //      @Override
+    public void onCameraViewStopped() {
+        frame.release();
+    }
+
+    public Bitmap getBmp(Mat tmp){
+
+        Bitmap bmp = null;
+        bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(tmp, bmp);
+
+        return bmp;
+    }
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    //    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        frame = inputFrame.rgba();
+        Mat dst = new Mat();
+        Core.flip(frame, dst, -1);
+
+        String encodedImage = getStringImage(getBmp(frame));
+        try {
+            Document doc = Jsoup.connect("http://192.168.250.1:5000/?img=" + encodedImage).get();
+            Elements ret = doc.select("#mp-itn b a");
+            System.out.println(ret.toString());
+        }
+        catch (java.io.IOException i){
+        }
+
+
+        System.out.println(getBmp(frame));
+
+        return frame;
+    }
+}
